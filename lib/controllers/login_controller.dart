d9638cd46ee0a64224f28771858fc51cd769592b
Lay <karomahlailatul@gmail.com>
2024-07-20 16:18:30 +0700

    added login api

--- lib/controllers/login_controller.dart
+++ lib/controllers/login_controller.dart
@@ -10,11 +10,31 @@
 //   }
 // }
 
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class LoginController {
-  TextEditingController phoneController = TextEditingController();
+   TextEditingController phoneController = TextEditingController();
 
+  Future<bool> login() async {
+    final response = await http.post(
+      Uri.parse('https://shopers-v2-be.vercel.app/api/login'),
+      headers: {'Content-Type': 'application/json'},
+      body: jsonEncode({
+        'phone': phoneController.text,
+      }),
+    );
+
+    if (response.statusCode == 200) {
+      final responseBody = jsonDecode(response.body);
+      if (responseBody['success']) {
+        return true;
+      }
+    }
+    return false;
+  }
+  
   void onClose() {
     phoneController.dispose();
   }
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -11,176 +11,196 @@ class LoginPage extends StatefulWidget {
 
 class _LoginPageState extends State<LoginPage> {
   bool? isChecked = false;
-  final LoginController loginController =
-      LoginController(); // Inisialisasi di sini
+  final LoginController loginController = LoginController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: const Color(0xffffffff),
-        body: SafeArea(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20),
-            child: ListView(
-              children: [
-                const SizedBox(height: 50),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Image.asset('assets/images/login_image1.png'),
-                    const SizedBox(height: 45),
-                    const Text(
-                      'Silahkan masuk dengan nomor \ntelkomsel kamu',
-                      style: TextStyle(
-                          color: Color(0xff1E272E),
-                          fontSize: 18,
-                          fontWeight: FontWeight.bold),
+      backgroundColor: const Color(0xffffffff),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: ListView(
+            children: [
+              const SizedBox(height: 50),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Image.asset('assets/images/login_image1.png'),
+                  const SizedBox(height: 45),
+                  const Text(
+                    'Silahkan masuk dengan nomor \ntelkomsel kamu',
+                    style: TextStyle(
+                      color: Color(0xff1E272E),
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
                     ),
-                    const SizedBox(height: 25),
-                    const Text(
-                      'Nomor HP',
-                      style: TextStyle(
-                          fontSize: 14,
-                          fontWeight: FontWeight.bold,
-                          color: Color(0xff1E272E)),
+                  ),
+                  const SizedBox(height: 25),
+                  const Text(
+                    'Nomor HP',
+                    style: TextStyle(
+                      fontSize: 14,
+                      fontWeight: FontWeight.bold,
+                      color: Color(0xff1E272E),
                     ),
-                    TextField(
-                      controller: loginController.phoneController,
-                      keyboardType: TextInputType.phone,
-                      autocorrect: false,
-                      decoration: InputDecoration(
-                          hintText: 'Cth. 08129011xxxx',
-                          hintStyle: const TextStyle(
-                              color: Color(0xffA4B0BE),
-                              fontSize: 14,
-                              fontWeight: FontWeight.w500),
-                          enabledBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(4),
-                            borderSide:
-                                const BorderSide(color: Color(0xffA4B0BE)),
-                          ),
-                          focusedBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(4),
-                            borderSide:
-                                const BorderSide(color: Color(0xffA4B0BE)),
-                          ),
-                          contentPadding: const EdgeInsets.symmetric(
-                              horizontal: 10, vertical: 3)),
+                  ),
+                  TextField(
+                    controller: loginController.phoneController,
+                    keyboardType: TextInputType.phone,
+                    autocorrect: false,
+                    decoration: InputDecoration(
+                      hintText: 'Cth. 08129011xxxx',
+                      hintStyle: const TextStyle(
+                        color: Color(0xffA4B0BE),
+                        fontSize: 14,
+                        fontWeight: FontWeight.w500,
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(4),
+                        borderSide: const BorderSide(color: Color(0xffA4B0BE)),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(4),
+                        borderSide: const BorderSide(color: Color(0xffA4B0BE)),
+                      ),
+                      contentPadding: const EdgeInsets.symmetric(horizontal: 10, vertical: 3),
                     ),
-                    const SizedBox(height: 10),
-                    Row(
-                      children: [
-                        Expanded(
-                          flex: 1,
-                          child: Checkbox(
-                            value: isChecked,
-                            activeColor: const Color(0xffEC2028),
-                            onChanged: (newBool) {
-                              setState(() {
-                                isChecked = newBool;
-                              });
-                            },
-                          ),
+                  ),
+                  const SizedBox(height: 10),
+                  Row(
+                    children: [
+                      Expanded(
+                        flex: 1,
+                        child: Checkbox(
+                          value: isChecked,
+                          activeColor: const Color(0xffEC2028),
+                          onChanged: (newBool) {
+                            setState(() {
+                              isChecked = newBool;
+                            });
+                          },
                         ),
-                        const SizedBox(width: 10),
-                        Expanded(
-                          flex: 20,
-                          child: RichText(
-                              text: const TextSpan(
-                                  style: TextStyle(
-                                      color: Color(0xff1E272E), fontSize: 14),
-                                  children: <TextSpan>[
-                                TextSpan(text: 'Saya menyetujui '),
-                                TextSpan(
-                                  text: 'syarat, ketentuan, ',
-                                  style: TextStyle(
-                                    color: Color(0xffEC2028),
-                                  ),
-                                ),
-                                TextSpan(text: 'dan '),
-                                TextSpan(
-                                  text: 'privasi ',
-                                  style: TextStyle(
-                                    color: Color(0xffEC2028),
-                                  ),
-                                ),
-                                TextSpan(text: '\nTelkomsel')
-                              ])),
-                        ),
-                      ],
-                    ),
-                    const SizedBox(height: 20),
-                    GestureDetector(
-                      onTap: () {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => const HomePage()));
-                      },
-                      child: Center(
-                        child: Container(
-                          height: 42,
-                          width: MediaQuery.of(context).size.width,
-                          decoration: BoxDecoration(
-                            color: const Color(0xffEC2028),
-                            borderRadius: BorderRadius.circular(4),
-                          ),
-                          child: const Center(
-                            child: Text(
-                              'LANJUT',
-                              style: TextStyle(
-                                color: Color(0xffffffff),
-                                fontSize: 14,
-                                fontWeight: FontWeight.bold,
+                      ),
+                      const SizedBox(width: 10),
+                      Expanded(
+                        flex: 20,
+                        child: RichText(
+                          text: const TextSpan(
+                            style: TextStyle(color: Color(0xff1E272E), fontSize: 14),
+                            children: <TextSpan>[
+                              TextSpan(text: 'Saya menyetujui '),
+                              TextSpan(
+                                text: 'syarat, ketentuan, ',
+                                style: TextStyle(color: Color(0xffEC2028)),
                               ),
+                              TextSpan(text: 'dan '),
+                              TextSpan(
+                                text: 'privasi ',
+                                style: TextStyle(color: Color(0xffEC2028)),
+                              ),
+                              TextSpan(text: '\nTelkomsel'),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 20),
+                  GestureDetector(
+                    onTap: () async {
+                      bool success = await loginController.login();
+                      if (success) {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => const HomePage(),
+                          ),
+                        );
+                      } else {
+                        showDialog(
+                          context: context,
+                          builder: (context) {
+                            return AlertDialog(
+                              title: const Text('Login Gagal'),
+                              content: const Text('Nomor telepon atau kata sandi salah.'),
+                              actions: [
+                                TextButton(
+                                  onPressed: () {
+                                    Navigator.of(context).pop();
+                                  },
+                                  child: const Text('OK'),
+                                ),
+                              ],
+                            );
+                          },
+                        );
+                      }
+                    },
+                    child: Center(
+                      child: Container(
+                        height: 42,
+                        width: MediaQuery.of(context).size.width,
+                        decoration: BoxDecoration(
+                          color: const Color(0xffEC2028),
+                          borderRadius: BorderRadius.circular(4),
+                        ),
+                        child: const Center(
+                          child: Text(
+                            'LANJUT',
+                            style: TextStyle(
+                              color: Color(0xffffffff),
+                              fontSize: 14,
+                              fontWeight: FontWeight.bold,
                             ),
                           ),
                         ),
                       ),
                     ),
-                    const SizedBox(height: 15),
-                    const Center(
-                      child: Text(
-                        'Atau masuk menggunakan',
-                        style:
-                            TextStyle(color: Color(0xff747D8C), fontSize: 14),
-                      ),
+                  ),
+                  const SizedBox(height: 15),
+                  const Center(
+                    child: Text(
+                      'Atau masuk menggunakan',
+                      style: TextStyle(color: Color(0xff747D8C), fontSize: 14),
                     ),
-                    const SizedBox(height: 15),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        GestureDetector(
-                          onTap: () {},
-                          child: Container(
-                            width: 157,
-                            height: 42,
-                            decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(4),
-                                border:
-                                    Border.all(color: const Color(0xff3B5998))),
-                            child: Image.asset('assets/icons/facebook.png'),
+                  ),
+                  const SizedBox(height: 15),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      GestureDetector(
+                        onTap: () {},
+                        child: Container(
+                          width: 157,
+                          height: 42,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(4),
+                            border: Border.all(color: const Color(0xff3B5998)),
                           ),
+                          child: Image.asset('assets/icons/facebook.png'),
                         ),
-                        GestureDetector(
-                          onTap: () {},
-                          child: Container(
-                            width: 157,
-                            height: 42,
-                            decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(4),
-                                border:
-                                    Border.all(color: const Color(0xff1DA1F2))),
-                            child: Image.asset('assets/icons/twitter.png'),
+                      ),
+                      GestureDetector(
+                        onTap: () {},
+                        child: Container(
+                          width: 157,
+                          height: 42,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(4),
+                            border: Border.all(color: const Color(0xff1DA1F2)),
                           ),
+                          child: Image.asset('assets/icons/twitter.png'),
                         ),
-                      ],
-                    )
-                  ],
-                ),
-              ],
-            ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ],
           ),
-        ));
+        ),
+      ),
+    );
   }
 }
